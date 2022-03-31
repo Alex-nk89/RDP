d34ecaddb234dfb2da7d27f7584eeaca0cc2f9ed
Нагайцев Александр Евгеньевич <NagaytsevAE@nknh.ru>
2022-03-31 13:49:42 +0300

    Конфигуратор мнемосхем. Зуммирование холста

--- RealtimeDataPortal/ClientApp/src/components/configurator/forms/mnemoscheme/EditMnemoscheme.jsx
+++ RealtimeDataPortal/ClientApp/src/components/configurator/forms/mnemoscheme/EditMnemoscheme.jsx
@@ -1,5 +1,5 @@
 import {
-    useState,
+    useState, useEffect,
     useSelector,
     Space, Stepper,
     useNotification, useRequest,
@@ -28,6 +28,73 @@ export const EditMnemoscheme = ({ action, form, nameRef, submitForm, addAccessIc
     };
 
     const nextStep = () => setActiveStep((current) => (current < 2 ? current + 1 : current));
+
+    const zoomMnemoscheme = (event) => {
+        const delta = event.e.deltaY;
+        let zoom = mnemoscheme.getZoom();
+        zoom *= 0.999 ** delta;
+        if (zoom > 5) zoom = 5;
+        if (zoom < 0.1) zoom = 0.1;
+        mnemoscheme.zoomToPoint({ x: event.e.offsetX, y: event.e.offsetY }, zoom);
+        event.e.preventDefault();
+        event.e.stopPropagation();
+    };
+
+    function grabCanvas (event) {
+        const evt = event.e;
+        if (evt.altKey === true) {
+          this.isDragging = true;
+          this.selection = false;
+          this.lastPosX = evt.clientX;
+          this.lastPosY = evt.clientY;
+        }
+    };
+
+    function moveCanvas (event) {
+        if (this.isDragging) {
+            const e = event.e;
+            let vpt = this.viewportTransform;
+            vpt[4] += e.clientX - this.lastPosX;
+            vpt[5] += e.clientY - this.lastPosY;
+            this.requestRenderAll();
+            this.lastPosX = e.clientX;
+            this.lastPosY = e.clientY;
+          }
+    };
+
+    function letGoCanvas () {
+        this.setViewportTransform(this.viewportTransform);
+        this.isDragging = false;
+        this.selection = true;
+    };
+
+    useEffect(() => {
+        mnemoscheme?.on('mouse:wheel', zoomMnemoscheme);
+
+        return mnemoscheme?.on('mouse:wheel', zoomMnemoscheme);
+        //eslint-disable-next-line
+    }, [mnemoscheme]);
+
+    useEffect(() => {
+        mnemoscheme?.on('mouse:down', grabCanvas);
+
+        return mnemoscheme?.on('mouse:down', grabCanvas);
+        //eslint-disable-next-line
+    }, [mnemoscheme]);
+
+    useEffect(() => {
+        mnemoscheme?.on('mouse:move', moveCanvas);
+
+        return mnemoscheme?.on('mouse:move', moveCanvas);
+        //eslint-disable-next-line
+    }, [mnemoscheme]);
+
+    useEffect(() => {
+        mnemoscheme?.on('mouse:up', letGoCanvas);
+
+        return mnemoscheme?.on('mouse:up', letGoCanvas);
+        //eslint-disable-next-line
+    }, [mnemoscheme]);
 
     return (
         <>
